b758c662eef22288e3d6bb86403c5aae32d8fe7f
hayeon17kim <qwndhs@gmail.com>
2021-01-30 22:57:25 +0900

    docs: add default value: now() to every rdt in db

--- src/main/java/com/hiwork/config/WebMvcConfig.java
+++ src/main/java/com/hiwork/config/WebMvcConfig.java
@@ -1,18 +1,19 @@
 package com.hiwork.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.hiwork.interceptor.AutoLoginInterceptor;
-import com.hiwork.service.WorkerService;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
   @Autowired
-  WorkerService workerService;
+  AutoLoginInterceptor autoLoginInterceptor;
   
   @Override 
   public void addInterceptors(InterceptorRegistry registry) { 
-    registry.addInterceptor(new AutoLoginInterceptor(workerService));
+    registry.addInterceptor(autoLoginInterceptor);
   }
 
 }
