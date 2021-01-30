46b4e2ee1ab459d24f7ff1297dd8db0fcb64d213
ryu seunghee <ryuseunghee.dev@gmail.com>
2021-01-30 17:56:10 +0900

    merge

--- src/main/java/com/hiwork/repository/BoardRepository.java
+++ src/main/java/com/hiwork/repository/BoardRepository.java
@@ -1,6 +1,5 @@
 package com.hiwork.repository;
 
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -10,4 +9,5 @@ public interface BoardRepository extends JpaRepository<Board, Integer> {
   @Modifying
   @Query("UPDATE Board b SET b.viewCount = b.viewCount + 1 where b.no = :no")
   void updateViewCount(int no);
+
 }
